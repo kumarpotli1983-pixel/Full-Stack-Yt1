214a39ff936fa9a5e870fb83c62d1e99b3cfdb7c
kumarpotli1983-pixel <kumarpotli1983@gmail.com>
2026-07-17 17:24:34 +0530

    add getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -422,6 +422,54 @@ const getUserChannelProfile = asyncHandler(async (req,res)=>{
             .json(new ApiResponse(200,channel[0],"User channel fetched succesfully"))
 })
 
+const getWatchHistory = asyncHandler(async(req,res)=>
+{
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id:new mongoose.Schema.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"videos",
+        localField: "watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullname:1,
+                    username:1,
+                    avatar:1
+                  }
+                }
+              ]
+
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res.status(200)
+            .json(new ApiResponse(200,user[0].watchHistory,"watchHistory fetched succesfully"))
+})
 export {loginUser}
 export {registerUser}
 export {logoutUser}
@@ -432,5 +480,7 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory
 }
